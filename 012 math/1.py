229c7eeffb075f5a59269e9c12944857725621b2
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-10 03:01:21 +0400

    012 math

---
+++ 012 math/1.py
@@ -0,0 +1,13 @@
+# Python da riyazi emeliyyatlar üçün xüsusi metodlar vardır
+
+# onlardan biri math metodudur.
+
+# math metodunu işletmek üçün import ( daxil ) etmək lazımdır 
+
+# əğər import etməsək onda terminalda müəyyən edilməmiş diyə xəta mesajı çıxacaq.
+ 
+
+import math
+pi = 3.14
+
+print( math.ceil(pi) )
---
+++ 012 math/2.py
@@ -0,0 +1,7 @@
+# import etdikden sonra math dan sonra nogte qoysaq 
+# math ile istifade ede bileceyimiz meton adlarını göreceyig.
+
+import math
+pi = 3.14
+
+print( math.ceil(pi) )
