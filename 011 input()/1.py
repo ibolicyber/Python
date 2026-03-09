8ec67ba9640d13bb373775cd6cbeb1864f409b54
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-10 02:44:44 +0400

    011 input()

---
+++ 011 input()/1.py
@@ -0,0 +1,7 @@
+# input() bu metod sahe yaradır ve onun içine isdediğimizi yaza bileriq
+# Hemin sahe içinde yazdıqlarımızı VARİABLE yaddaşına atırıq və
+# print ilə onu ekrana çap edirik
+
+ad = input("Sənin adın nədir: ")
+
+print( str(ad) )
---
+++ 011 input()/2.py
@@ -0,0 +1,2 @@
+# input içerisinde sadəcə rəgəm yazılmalıdırsa if ilə onu edə bilərik
+# if in nece istifade edilmeli olduğu sonraki folderde izah edilib..
