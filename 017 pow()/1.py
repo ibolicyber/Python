8623862ebbdc201a4be2c6e605d5956db18cea4a
ibolicyber <ibrahimcyber4@gmail.com>
2026-03-12 16:30:40 +0400

    017 pow()

---
+++ 017 pow()/1.py
@@ -0,0 +1,7 @@
+# ədədi qüvvete yükseltmek üçün " x ** y " istifadə etmek mümkündür
+# birde ededi qüvvete yükseltmek üçün "pow()" funksiyasından istifadə edilir.
+# 2 parametr gebul edir: 1 - ededi, 2 - hemin ededin üst derecesi
+
+
+eded = 2
+print( pow(eded , 5) )    # 2 * 2 * 2 * 2 * 2 = 32
---
+++ 017 pow()/2.py
@@ -0,0 +1,7 @@
+# əğər import math etsək arada bir ferg olacaq 
+
+import math
+eded = 2
+
+print( pow( eded , 5 ) )       # 32 
+print( math.pow( eded, 5 ) )   # 32.0
